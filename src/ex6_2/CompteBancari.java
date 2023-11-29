290b28260d99a740ac235938f8c3b0626e6343a0
erikmozos <mozoskp@gmail.com>
2023-11-29 19:46:14 +0100

    Faltava classe CompteBancari

--- src/ex6_2/CompteBancari.java
+++ src/ex6_2/CompteBancari.java
@@ -1,5 +1,65 @@
 package ex6_2;
 
 public class CompteBancari {
+	
+	private String dniTitular;
+	private String nomTitular;
+	private double saldo;
+	
+	
+	//constructor
+	
+	public CompteBancari() {
+		
+	}
+	
+	public CompteBancari(String dniTitular, String nomTitular, int saldo) {
+		this.dniTitular = dniTitular;
+		this.nomTitular = nomTitular;
+		this.saldo = saldo;
+	}
+	
+	public void ingressar() {
+		
+	}
+	
+	public void treuresaldo() {
+		
+	}
+	
+	public void mostrarsaldo(){
+		
+	}
+
+	
+	
+	//getter setter
+	
+	public String getDniTitular() {
+		return dniTitular;
+	}
+
+	public void setDniTitular(String dniTitular) {
+		this.dniTitular = dniTitular;
+	}
+
+	public String getNomTitular() {
+		return nomTitular;
+	}
+
+	public void setNomTitular(String nomTitular) {
+		this.nomTitular = nomTitular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+	
 
 }
