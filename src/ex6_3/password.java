dda9a1e0269f68dcc37519cee85fa6b46270f438
erikmozos <mozoskp@gmail.com>
2023-12-01 17:44:52 +0100

    Programa 6.3 acabat

--- src/ex6_3/password.java
+++ src/ex6_3/password.java
@@ -1,37 +1,78 @@
 package ex6_3;
 import java.util.Random;
-public class password {
-	
+public class Password {
+
 	Random rand = new Random();
-	private char Contrassenya[] = new char[this.Longitud];
-	private int Longitud = 8;
-	
-	public password() {
-		
+	private String Contrassenya= "";
+	private int Longitud = 0;
+	private boolean Esrobust;
+
+	public Password(int Longitud) {
+		this.Longitud = Longitud;
 		int nombrerandom[] = new int[this.Longitud] ;
-		
-		
+
+
 		for(int i = 0; i < Longitud; i++) {
-			nombrerandom[i] = rand.nextInt((165 - 48)+1)+48;
-			char caracter = (char) nombrerandom[i];
-			this.Contrassenya[i] = caracter; 
+			nombrerandom[i] = rand.nextInt((126 - 48)+1)+48;
+			char caracter= (char) nombrerandom[i];
+			Contrassenya = Contrassenya + caracter; 
+		}
+	}
+
+	public boolean isEsrobust() {
+		return Esrobust;
+	}
+
+	public void setEsrobust(boolean esrobust) {
+		Esrobust = esrobust;
+	}
+
+	public Password() {
+		Longitud = 8;
+		int nombrerandom[] = new int[Longitud] ;
+
+
+		for(int i = 0; i < Longitud; i++) {
+			nombrerandom[i] = rand.nextInt((126 - 48)+1)+48;
+			char caracter= (char) nombrerandom[i];
+			Contrassenya = Contrassenya + caracter; 
 		}
 		
 	}
 	
-	public static void Contrassenya() {
+	public void Esrobust() {
+		boolean majus = false;
+		boolean minus = false;
+		boolean number = false;
+		boolean simbol = false;
+		for(int i= 0; i< Contrassenya.length(); i++) {
+		if(Character.isUpperCase(Contrassenya.charAt(i))) {
+			majus = true;
+		}
+		if(Character.isLowerCase(Contrassenya.charAt(i))) {
+			minus = true;
+		}
 		
-	}
-	
-	public static void Longitud() {
-		
+		if(Character.isDigit(Contrassenya.charAt(i))) {
+			number = true;
+		}
+		if(!(Character.isLetterOrDigit(Contrassenya.charAt(i)))) {
+			simbol = true;
+		}
+		}
+		if((majus == true) && (minus == true) && (number == true) && (simbol == true)) {
+			this.Esrobust = true;
+		}
+		else {
+			this.Esrobust = false;
+		}
 	}
 
-	public char[] getContrassenya() {
+	public String getContrassenya() {
 		return Contrassenya;
 	}
 
-	public void setContrassenya(char[] contrassenya) {
+	public void setContrassenya(String contrassenya) {
 		Contrassenya = contrassenya;
 	}
 
@@ -42,6 +83,9 @@ public class password {
 	public void setLongitud(int longitud) {
 		Longitud = longitud;
 	}
-	
-	
+
+
+
+
+
 }
